7c494d4e400fb9ff0ec8a9bd862e8c8000682e68
MartinKouba5 <128465955+MartinKouba5@users.noreply.github.com>
2024-03-10 22:18:31 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { fetchWeatherApi } from 'openmeteo';
+import WeatherCard from './WeatherCard'; // cesta k souboru WeatherCard
 import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <WeatherCard cityName="Žatec" latitude={50.3272} longitude={13.5458} backgroundImage="https://cdn.kudyznudy.cz/files/51/51f3d2b6-530a-4d6d-97bf-e9d05518b3ec.webp?v=20230919103400" />
+      <WeatherCard cityName="Louny" latitude={50.357} longitude={13.7967} backgroundImage="https://noviny-zblizka.cz/wp-content/uploads/2022/03/Chram-Mikulas-Louny.jpg" />
+      <WeatherCard cityName="Most" latitude={50.503} longitude={13.6362} backgroundImage="https://www.imostecko.cz/src/uploads/petr-toman-nepouzivat-na-pohlednice-16.jpg" />
+      <WeatherCard cityName="Praha" latitude={50.088} longitude={14.4208} backgroundImage="https://images8.alphacoders.com/374/374028.jpg" />
+      <WeatherCard cityName="Brno" latitude={49.1952} longitude={16.608} backgroundImage="https://media.istockphoto.com/id/1355785866/photo/a-rare-view-of-brno.jpg?s=612x612&w=0&k=20&c=ssEPuXC7DzwIBgoX8dIksH4U1nKTZMwaXaD-KpDG2bQ=" />
+      <WeatherCard cityName="Ostrava" latitude={49.8347} longitude={18.282} backgroundImage="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Masarykovo_n%C3%A1m%C4%9Bst%C3%AD_v_Ostrav%C4%9B.jpg/1200px-Masarykovo_n%C3%A1m%C4%9Bst%C3%AD_v_Ostrav%C4%9B.jpg" />
 
+   
+    </div>
+    
+
+);
+}
 export default App;
---
+++ src/WeatherCard.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import './WeatherCard.css';
+
+function WeatherCard({ cityName, latitude, longitude,backgroundImage }) {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`)
+      .then(response => response.json())
+      .then(data => {
+        const currentWeather = {
+          temperature: data.hourly.temperature_2m[0],
+          humidity: data.hourly.relative_humidity_2m[0],
+          windSpeed: data.hourly.wind_speed_10m[0]
+        };
+        setCurrentWeather(currentWeather);
+
+        const forecast = data.hourly.temperature_2m.slice(1, 8).map((temp, index) => ({
+          temperature: temp,
+          humidity: data.hourly.relative_humidity_2m[index + 1],
+          windSpeed: data.hourly.wind_speed_10m[index + 1]
+        }));
+        setForecast(forecast);
+      })
+      .catch(error => console.error('Error:', error));
+  }, [latitude, longitude]);
+
+  return (
+    <div className="WeatherCard" style={{ backgroundImage: `url(${backgroundImage})` }} >
+      <h2>{cityName}</h2>
+      {currentWeather ? (
+        <div>
+          <h3><strong><svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="currentColor" class="bi bi-thermometer" viewBox="0 0 16 16">
+  <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+  <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0M6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15z"/>
+</svg> {Math.round(currentWeather.temperature * 100) / 100}°C</strong></h3>
+          <p><strong><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
+  <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
+</svg> {Math.round(currentWeather.humidity * 100) / 100}%</strong></p>
+          <p><strong><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+  <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
+</svg> {Math.round(currentWeather.windSpeed * 100) / 100} m/s</strong></p>
+        </div>
+      ) : (
+        <p>Načítání dat...</p>
+      )}
+      {forecast ? (
+        
+        <div id="pozadi"style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {forecast.map((day, i) => {
+            const date = new Date(Date.now() + (i + 1) * 24 * 60 * 60 * 1000);
+            const formattedDate = `${date.getDate().toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}`;
+            return (
+              <div key={i}>
+                <div id="predpoved">
+                    
+                <p>{formattedDate}</p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer" viewBox="0 0 16 16">
+  <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+  <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0M6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15z"/>
+</svg> {Math.round(day.temperature * 100) / 100}°C</p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
+  <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>
+</svg> {Math.round(day.humidity * 100) / 100}%</p>
+                <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+  <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
+</svg> {Math.round(day.windSpeed * 100) / 100} m/s</p>
+              </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default WeatherCard;
